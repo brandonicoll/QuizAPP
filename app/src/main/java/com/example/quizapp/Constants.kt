bc2f9d9bff4c6feeed9e62ce3baf32ed7dd6da48
Brandon <brandonspersonalmailbox@gmail.com>
2021-06-25 13:23:46 -0400

    Finalized questions (20 in total)

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -102,6 +102,106 @@ object Constants {
 
         questionsList.add(que10)
 
+        //11
+        val que11 = Question(
+            11, "What species of fish is this?",
+            R.drawable.carp,
+            "Bass", "Catfish",
+            "Carp", "Ferenc", 3
+        )
+
+        questionsList.add(que11)
+
+        //12
+        val que12 = Question(
+            12, "What species of fish is this?",
+            R.drawable.pike,
+            "Walleye", "Pike",
+            "Muskellunge", "pickerel", 2
+        )
+
+        questionsList.add(que12)
+
+        //13
+        val que13 = Question(
+            13, "What species of fish is this?",
+            R.drawable.pike_perch,
+            "White Bass", "Carp",
+            "Sheephead", "Pike Perch", 4
+        )
+
+        questionsList.add(que13)
+
+        //14
+        val que14 = Question(
+            14, "What species of fish is this?",
+            R.drawable.largemouth_bass,
+            "White Bass", "Smallmouth Bass",
+            "Largemouth Bass", "Egyptian Bass", 3
+        )
+
+        questionsList.add(que14)
+
+        //15
+        val que15 = Question(
+            15, "What species of fish is this?",
+            R.drawable.tuna,
+            "Tuna", "Bonito Tuna",
+            "Skipjack Tuna", "False Albacore", 1
+        )
+
+        questionsList.add(que15)
+
+        //16
+        val que16 = Question(
+            16, "What species of fish is this?",
+            R.drawable.walleye,
+            "Pike", "Radiation Bass",
+            "Thanos", "Walleye", 4
+        )
+
+        questionsList.add(que16)
+
+        //17
+        val que17 = Question(
+            17, "What species of fish is this?",
+            R.drawable.white_grouper,
+            "Yellow Bass", "Grouper",
+            "White Grouper", "Yellow Fish", 3
+        )
+
+        questionsList.add(que17)
+
+        //18
+        val que18 = Question(
+            18, "What species of fish is this?",
+            R.drawable.yellow_perch,
+            "Perch", "Yellow Perch",
+            "Pike", "Yellow Pike", 2
+        )
+
+        questionsList.add(que18)
+
+        //19
+        val que19 = Question(
+            19, "What species of fish is this?",
+            R.drawable.yellowfin_grouper_tilapia,
+            "Yellowfin Grouper", "Yellowfin Tuna",
+            "Yellowfin Perch", "Yellowfin Mackerel", 1
+        )
+
+        questionsList.add(que19)
+
+        //20
+        val que20 = Question(
+            20, "What species of fish is this?",
+            R.drawable.mackerel,
+            "Blue Tuna", "Blue Pike",
+            "Atlantic Pike", "Mackerel", 4
+        )
+
+        questionsList.add(que20)
+
         return  questionsList
     }
 
