2e255dfaf333f5c06bbbfe0a6ac37148e137ce2e
Brandon <brandonspersonalmailbox@gmail.com>
2021-06-25 12:16:49 -0400

    Created QuestionsActivity.kt, Created questions class with with needed elements/members, then created constants file with a list of questions to be added to the array to be used later. (Still adding questions)

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,18 @@ class MainActivity : AppCompatActivity() {
 
         //Get rid of notification bar
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        btnStart.setOnClickListener {
+
+            if (et_text.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, QuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
     }
 }
