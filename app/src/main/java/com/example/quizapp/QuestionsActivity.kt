206fdbc9b22028e9a433de2d0ae9d37ac112b046
Brandon <brandonspersonalmailbox@gmail.com>
2021-06-26 09:05:00 -0400

    Made functions for setquestions and default text -> started implemented a onclick of each textview.

--- app/src/main/java/com/example/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuestionsActivity.kt
@@ -1,20 +1,43 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_questions.*
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener { //need View.OnClickListener if i use "this" for the onclick listener and implement a single onclick function below and use something like a if or when statement
+
+    private var mCurrentPosition : Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
 
-        val questionsList = Constants.getQuestions()
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
+        mQuestionsList = Constants.getQuestions()
 
-        progressBar.progress = currentPosition
-        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+        setQuestion()
+
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+
+    }
+
+    private fun setQuestion() {
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
+
+        defualtOptionsView()
+
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
 
         tvQ_question.text = question!!.question
         iv_image.setImageResource(question.image)
@@ -22,6 +45,24 @@ class QuestionsActivity : AppCompatActivity() {
         tv_option_two.text = question.optionTwo
         tv_option_three.text = question.optionThree
         tv_option_four.text = question.optionFour
+    }
+
+    private fun defualtOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3, tv_option_four)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+
+    }
+
+    override fun onClick(v: View?) {
 
     }
 }
