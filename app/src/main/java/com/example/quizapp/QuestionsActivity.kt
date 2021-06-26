71ae57dca1378bfbc3e57ca0ddb4fe03351da0b7
Brandon <brandonspersonalmailbox@gmail.com>
2021-06-26 08:43:22 -0400

    Populated the Views in the QuestionsActivity.kt

--- app/src/main/java/com/example/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuestionsActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_questions.*
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,18 @@ class QuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_questions)
 
         val questionsList = Constants.getQuestions()
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        tvQ_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+
     }
 }
